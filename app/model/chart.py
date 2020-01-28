c9f254122624dd99d02eafc8e7cfa7a86656b8ec
Rodrigo Fagundes <rmfagundes@gmail.com>
2020-01-28 09:56:46 -0300

    Allowing switching chart library.

--- app/model/chart.py
+++ app/model/chart.py
@@ -4,10 +4,12 @@ from model.thematic import Thematic
 import matplotlib.pyplot as plt
 from bokeh.plotting import figure, show, output_file
 from bokeh.embed import components
+from bokeh.io import export_png
 import io
 
 class Chart(BaseModel):
     ''' Model for fetching dinamic and static charts '''
+    CHART_LIB_DEF = {} # Defaults to BOKEH
     def get_chart(self, options):
         ''' Selects if the chart should be static or dynamic '''
         options['as_pandas'] = True
@@ -15,9 +17,14 @@ class Chart(BaseModel):
         dataframe = Thematic().find_dataset(options)
         chart = self.get_raw_chart(dataframe, options)
         
+        chart_lib = 'BOKEH'
+        # for chart_lib, chart_types in self.CHART_LIB_DEF.items():
+        #     if options['chart_type'] in chart_types:
+        #         if chart_lib == 'BOKEH':
+                    
         if options['as_image']:
-            return self.get_image(chart, options)
-        return self.get_dynamic_chart(chart, options)
+            return self.get_image(chart, chart_lib)
+        return self.get_dynamic_chart(chart, chart_lib)
     
     def get_raw_chart(self, dataframe, options):
         ''' Selects and loads the chart '''
@@ -25,26 +32,24 @@ class Chart(BaseModel):
             return self.draw_scatter(dataframe, options)
         pass
         
-    def get_image(self, chart, options):
+    @staticmethod
+    def get_image(chart, lib):
         ''' Gets chart as image '''
-        # /charts?theme=teindicadoresmunicipais&categorias=ds_agreg_primaria&valor=vl_indicador&agregacao=SUM&filtros=eq-cd_mun_ibge-2927408,and,eq-cd_indicador-%27te_nat_ocup_atual%27
+        if lib == 'BOKEH':
+            return export_png(chart, filename="chart.png")
+        pass
 
-        # bytes_image = io.BytesIO()
-        # chart.savefig(bytes_image, format='png')
-        # bytes_image.seek(0)
-        from bokeh.io import export_png
-
-        bytes_image = export_png(chart, filename="chart.png")
-        
-        return bytes_image
-
-    def get_dynamic_chart(self, chart, options):
+    @staticmethod
+    def get_dynamic_chart(chart, lib):
         ''' Gets dynamic chart '''
         # /charts?theme=teindicadoresmunicipais&categorias=ds_agreg_primaria&valor=vl_indicador&agregacao=SUM&filtros=eq-cd_mun_ibge-2927408,and,eq-cd_indicador-%27te_nat_ocup_atual%27
-        (script, div) = components(chart)
-        return {'script': script, 'div': div }
+        if lib == 'BOKEH':
+            (script, div) = components(chart)
+            return {'script': script, 'div': div }
+        pass
 
-    def draw_scatter(self, dataframe, options):
+    @staticmethod
+    def draw_scatter(dataframe, options):
         ''' Draws the scatterplot '''
         # http://localhost:5000/charts/scatter?theme=sstindicadoresestaduais&categorias=ds_agreg_primaria-v,ds_agreg_secundaria-x,vl_indicador-y&filtros=eq-nu_competencia-2017,and,eq-cd_indicador-%27sst_bene_sexo_idade_dias_perdidos%27&as_image=S
         colormap = {'Feminino': 'red', 'Masculino': 'blue', 'Indefinido': 'pink'}

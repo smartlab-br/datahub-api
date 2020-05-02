8c20918abaa6e3213d12eaf35c06aeb572a7281e
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2020-05-02 19:16:19 -0300

    Fixing default partitioning in analytical queries.

--- app/service/qry_options_builder.py
+++ app/service/qry_options_builder.py
@@ -4,8 +4,8 @@ class QueryOptionsBuilder():
     @classmethod
     def build_options(cls, r_args, rules = 'query'):
         ''' Constrói as opções da pesquisa '''
-        options = r_args.copy()
-
+        options = r_args.copy().to_dict(flat=False)
+        
         categorias = cls.extract_qry_param(r_args, 'categorias')
         if categorias is None:
             if rules in ['query']:

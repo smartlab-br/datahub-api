da397fe632f9d479121429a91de0b110bfb37d64
Rodrigo Fagundes <rodrigo.mfagundes@mpt.mp.br>
2020-07-15 13:47:17 -0300

    Adding ignore to external imports.

--- lint/run_linter.py
+++ lint/run_linter.py
@@ -4,7 +4,7 @@ from pylint import epylint as lint
 with open("./lint/report.txt", "w+") as FILE:
     (RESULT, ERROR) = lint.py_run(
         '. --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" \
-            --reports=n --score=n --output-format=parseable',
+            --reports=n --score=n --output-format=parseable --rcfile=./lint/.pylintrc',
         return_std=True
     )
     FILE.write(RESULT.getvalue())

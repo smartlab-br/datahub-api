c573d8ca6e5b23b2842c035bc60d40657138048c
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2020-07-15 07:42:31 -0300

    Uploading lint result as artifact

--- lint/run_linter.py
+++ lint/run_linter.py
@@ -1,17 +1,10 @@
 ''' Pylint runner '''
 from pylint import epylint as lint
 
-print(">>>>> Starting linter")
-(RESULT, ERROR) = lint.py_run(
-    '. --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" \
-        --reports=n --score=n --output-format=parseable',
-    return_std=True
-)
-print(RESULT.getvalue())
-
-print(">>>>> Opening report file")
-FILE = open("./lint/report.txt", "w+")
-print(">>>>> Writing report")
-FILE.write(RESULT.getvalue())
-FILE.close()
-print(">>>>> Done")
+with open("./lint/report.txt", "w+") as FILE:
+    (RESULT, ERROR) = lint.py_run(
+        '. --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" \
+            --reports=n --score=n --output-format=parseable',
+        return_std=True
+    )
+    FILE.write(RESULT.getvalue())

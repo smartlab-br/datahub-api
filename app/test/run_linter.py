5bfd904ad8b3bad676feb2d32325273d39aae702
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2020-07-16 15:06:36 -0300

    Checking current dir

--- app/test/run_linter.py
+++ app/test/run_linter.py
@@ -7,7 +7,7 @@ from pylint import epylint as lint
         --reports=n --score=y --output-format=parseable',
     return_std=True
 )
-
+print(os.path.abspath(__file__))
 DEST_DIR = f"{os.getenv('GITHUB_WORKSPACE')}/{os.getenv('INPUT_DEST')}"
 FILE = open(f"{DEST_DIR}/lint.txt", "w+")
 FILE.write(RESULT.getvalue())

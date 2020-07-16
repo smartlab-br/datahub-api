16f6964f16627f9fe437b6916e4ed98f0011049e
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2020-07-16 16:37:02 -0300

    removing user restriction

--- app/test/run_linter.py
+++ app/test/run_linter.py
@@ -4,18 +4,16 @@ from pylint import epylint as lint
 
 (RESULT, ERROR) = lint.py_run(
     '. --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" \
-        --reports=n --score=y --output-format=parseable',
+        --report=y --score=y --output-format=parseable',
     return_std=True
 )
-print(os.path.abspath(__file__))
-print(RESULT)
-print("================================")
-print(ERROR)
+
+while not RESULT.closed and not ERROR.closed:
+    pass
 
 DEST_DIR = f"{os.getenv('GITHUB_WORKSPACE')}/{os.getenv('INPUT_DEST')}"
 
 if not os.path.exists(DEST_DIR):
-    print('não existe')
     os.makedirs(DEST_DIR)
 
 FILE = open(f"{DEST_DIR}/lint.txt", "w+")

de74e931f9d60a6f03a385aca161d29d3dcc10ad
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2020-07-27 18:59:54 -0300

    Changing brackets some bracket index accessing to get method.

--- app/repository/empresa/empresa.py
+++ app/repository/empresa/empresa.py
@@ -24,12 +24,8 @@ class EmpresaRepository(HBaseRepository):
         result = {**result, **self.split_dataframe_by_perspective(result, options)}
 
         for ds_key in result:
-            col_cnpj_name = 'cnpj'
-            if ds_key in self.CNPJ_COLUMNS:
-                col_cnpj_name = self.CNPJ_COLUMNS[ds_key]
-            col_pf_name = None
-            if ds_key in self.PF_COLUMNS:
-                col_pf_name = self.PF_COLUMNS[ds_key]
+            col_cnpj_name = self.CNPJ_COLUMNS.get(ds_key, 'cnpj')
+            col_pf_name = self.PF_COLUMNS.get(ds_key)
 
             if not result[ds_key].empty:
                 result[ds_key] = self.filter_by_person(
@@ -38,12 +34,12 @@ class EmpresaRepository(HBaseRepository):
 
             # Redução de dimensionalidade (simplified)
             if not result[ds_key].empty and options.get('simplified'):
-                list_dimred = ['nu_cnpj_cei', 'nu_cpf', 'col_compet']
-                if ds_key in self.SIMPLE_COLUMNS:
-                    list_dimred = self.SIMPLE_COLUMNS[ds_key]
-                    # Garantir que col_compet sempre estará na lista
-                    if 'col_compet' not in list_dimred:
-                        list_dimred.append('col_compet')
+                list_dimred = self.SIMPLE_COLUMNS.get(
+                    ds_key, ['nu_cnpj_cei', 'nu_cpf', 'col_compet']
+                )
+                # Garantir que col_compet sempre estará na lista
+                if 'col_compet' not in list_dimred:
+                    list_dimred.append('col_compet')
                 result[ds_key] = result[ds_key][list_dimred]
 
             # Conversão dos datasets em json

fd4ed4b31f004016f94fd9a73b4aa16c144cc57b
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2021-01-08 08:35:18 -0300

    Adding readiness probe.

--- app/resources/v1/healthchecks.py
+++ app/resources/v1/healthchecks.py
@@ -1,9 +1,53 @@
-'''Classes de health check'''
+""" Classes de health check """
 from flask_restful import Resource
+import requests
+from flask import current_app
+from datasources import get_impala_connection, get_redis_pool
+
 
 class HCAlive(Resource):
-    ''' Classe para health check '''
+    """ Liveness probe class """
     @classmethod
     def get(cls):
-        ''' Metodo-base para health-check '''
+        """ Liveness probe """
         return {'message': 'OK'}
+
+
+class HCReady(Resource):
+    """ Readiness probe class """
+    @classmethod
+    def get(cls):
+        """ Readiness probe """
+        probe_analysis = {**cls.probe_databases()}
+        if all(probe_analysis.values()):
+            return probe_analysis
+        return probe_analysis, 406
+
+    @staticmethod
+    def probe_databases():
+        """ Checks if databases are up """
+        result = {"impala": False, "hbase": False, "redis": False}
+        try:
+            if get_impala_connection():
+                result["impala"] = True
+        except:
+            pass
+        try:
+            if get_redis_pool():
+                result["redis"] = True
+        except:
+            pass
+        try:
+            url = "http://{}:{}/{}/{}".format(
+                current_app.config["HBASE_HOST"],
+                current_app.config["HBASE_PORT"],
+                "empresa",
+                "schema"
+            )
+            response = requests.get(url, headers={'Accept': 'application/json'})
+            # If the response was successful, no Exception will be raised
+            response.raise_for_status()
+            result["hbase"] = True
+        except:
+            pass
+        return result

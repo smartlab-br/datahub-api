5ed2410a9f4187bdb17a95b14a25e46320a0deac
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2021-02-23 13:37:47 -0300

    Explorign graphql querying.

--- app/model/mapbiomas/car.py
+++ app/model/mapbiomas/car.py
@@ -5,18 +5,30 @@ import requests
 
 class Car(Thematic):
     """ Model for getting report from mapbiomas and adding more data """
-    def find_by_car(self, car):
+    def find_by_car_and_alert(self, car, alert):
         """ Gather data from different sources and put them together """
         car = '271752'
-        report = self.fetch_report_from_source(car) \
-            .replace("/static/", "https://plataforma.alerta.mapbiomas.org/") \
-            .replace("http://localhost:5000/site.webmanifest", "https://plataforma.alerta.mapbiomas.org/site.webmanifest")
+        report = self.fetch_report_from_source(car, alert)
         # car_data = self.find_dataset({})
 
         return report
 
-    def fetch_report_from_source(self, car):
+    def fetch_report_from_source(self, car, alert):
         """ Get the HTML report from MapBiomas """
-        resp = requests.get(f"https://plataforma.alerta.mapbiomas.org/laudos/{car}", verify=False)
+        alert = 90340
+        car = 3618853
+        gql_qry = f"""alertReport(alertId:{alert}, carId:{car}) {{
+                        alertAreaInCar
+                        carCode
+                        images {{
+                          alertInProperty
+                          propertyInState
+                        }}
+                      }}"""
+        resp = requests.post(
+            f"https://plataforma.alerta.mapbiomas.org/app/graphql",
+            json={'query': gql_qry},
+            verify=False
+        )
         resp.raise_for_status()
-        return resp.text
+        return resp

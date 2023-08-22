c83cb743230c5d7fd7d8460fd897913fe948516a
Daniela de Miranda Guerra <daniela.guerra@mpt.mp.br>
2023-08-22 16:09:15 -0300

    chore: removing comments

--- app/repository/empresa/datasets.py
+++ app/repository/empresa/datasets.py
@@ -7,28 +7,6 @@ class DatasetsRepository(RedisRepository):
     ''' Definição do repo '''
     REDIS_KEY = 'rx:ds'
 
-    # TODO Na migração para configmap no k8s essa conf deve ser adicionada no configmap
-    # DATASETS_COMPETENCIA = current_app.config["CONF_REPO_DATASETS_COMPETENCIA"]
-    # DATASETS_COMPETENCIA = {
-    #     "auto": "DISTINCT ano",
-    #     "caged": "DISTINCT competencia_declarada",
-    #     "cagedano": "DISTINCT ano",
-    #     "cagedsaldo": "DISTINCT competencia_mov",
-    #     "cagedtrabalhador": "DISTINCT competencia_mov",
-    #     "cagedtrabalhadorano": "DISTINCT ano_declarado",
-    #     "rais": "DISTINCT nu_ano_rais",
-    #     "catweb_c": "DISTINCT ano",
-    #     "catweb": "DISTINCT ano_cat",
-    #     "rfb": "MAX dt_carga",
-    #     "sisben_c": "DISTINCT nu_ano_compet",
-    #     "sisben": "DISTINCT ano_beneficio",
-    #     "rfbsocios": "MAX dt_carga",
-    #     "rfbparticipacaosocietaria": "MAX dt_carga",
-    #     "renavam": "MAX dt_carga",
-    #     "aeronaves": "MAX dt_carga",
-    #     "embarcacoes": "MAX dt_carga"
-    # }
-
     def retrieve(self):
         ''' Localiza o dicionário de datasources no REDIS '''
         return self.retrieve_hashset(self.REDIS_KEY)

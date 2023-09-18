08d120862e6ef893dcaaf1069d3550713ec4c40d
Daniela de Miranda Guerra <daniela.guerra@mpt.mp.br>
2023-09-18 15:13:57 -0300

    chore: removing unused source models

--- app/factory/source.py
+++ app/factory/source.py
@@ -1,15 +1,6 @@
 ''' Class for instantiating query options builder objects '''
 from model.source.base import BaseSource
 from model.source.caged import CagedSaldo
-# from model.source.aeronaves import Aeronaves
-# from model.source.auto import Auto
-# from model.source.catweb import Catweb
-# from model.source.rais import Rais
-# from model.source.renavam import Renavam
-# from model.source.sisben import Sisben
-# from model.source.rfb import BaseRfb, RfbSocios, RfbParticipacaoSocietaria
-# from model.source.rfb import RfbParticipacaoSocietaria
-# from model.source.embarcacoes import Embarcacoes
 
 class SourceFactory():
     ''' Factory for instantiating specific models depending on the source
@@ -21,32 +12,3 @@ class SourceFactory():
             return CagedSaldo()
         # Default
         return BaseSource()
-
-        # if dataframe == 'aeronaves':
-        #     return Aeronaves()
-        # if dataframe == 'auto':
-        #     return Auto()
-        # if dataframe in ['catweb', 'catweb_c']:
-        #     return Catweb()
-        # if dataframe == 'rais':
-        #     return Rais()
-        # if dataframe == 'renavam':
-        #     return Renavam()
-        # if dataframe in ['sisben', 'sisben_c']:
-        #     return Sisben()
-        # RFB
-        # if dataframe == 'rfb':
-        #     return BaseRfb()
-        # if dataframe == 'rfbsocios':
-        #     return RfbSocios()
-        # if dataframe == 'rfbparticipacaosocietaria':
-        #     return RfbParticipacaoSocietaria()
-        # CAGED
-        # if dataframe == 'cagedsaldo':
-        #     return CagedSaldo()
-        # if dataframe is not None and 'caged' in dataframe:
-        #     if dataframe == 'cagedsaldo':
-        #         return CagedSaldo()
-        #     return BaseCaged()
-        # if dataframe == 'embarcacoes':
-        #     return Embarcacoes()
